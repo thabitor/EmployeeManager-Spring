18dffb6a49d5e553f1b169ece5048888a282c3ca
thabitor <thabithamror@gmail.com>
2022-08-24 15:56:57 +0200

    connected to db

---
+++ src/main/java/code/tfn/employeemanager/repositories/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package code.tfn.employeemanager.repositories;
+
+import code.tfn.employeemanager.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
